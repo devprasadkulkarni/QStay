1a044b93f453a6d84e6869252f3912665e9855d2
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2026-01-07 18:45:25 +0530

    feat:featured destinations ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import FeaturedDestinations from "../components/FeaturedDestinations";
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <FeaturedDestinations />
         </div>
     );
 };
